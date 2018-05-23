588c07957b717c10c44df0365c306d9aa2cfb6df
Training_C2a.05.01 <Training_C2a.05.01@accenture.com>
2018-05-23 12:18:17 +0530

    test code change add slash

--- src/test/java/com/tests/NewTest2.java
+++ src/test/java/com/tests/NewTest2.java
@@ -17,7 +17,7 @@ public class NewTest2 {
 	@Test
 	public void launchChrome()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\Program Files (x86)\Google\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.get("http://newtours.demoaut.com/");
 		driver.manage().window().maximize();
